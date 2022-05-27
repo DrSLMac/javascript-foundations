67205e28406cd853cba572d4525f1cad1a03791b
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2022-05-26 23:04:11 -0500

    rework unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,16 +1,67 @@
 class Unicorn {
-  constructor(name, color, saying) {
+  constructor(name, color, says) {
     this.name = name;
     this.color = color || 'white';
+    this.saying = '';
   }
-  isWhite() {
-    if (this.color !== 'white') {
+  isWhite(color) {
+    //if unicorn is white, then true
+    //if unicorn is not white, then false
+    if ( this.color === 'white') {
+      return true;
+    } else {
       return false;
     }
   }
-  says(message) {
-    return `**;* ${message} *;**`;
+  says(saying) {
+      return `**;* ${saying} *;**`;
+    }
   }
-}
+
+
 
 module.exports = Unicorn;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Unicorn {
+//   constructor(name, color, saying) {
+//     this.name = name;
+//     this.color = color || 'white';
+//   }
+//   isWhite() {
+//     if (this.color !== 'white') {
+//       return false;
+//     }
+//   }
+//   says(message) {
+//     return `**;* ${message} *;**`;
+//   }
+// }
