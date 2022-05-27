d0d092f47b30510a559754e68a44b7fed7fd72c7
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2022-05-26 23:34:13 -0500

    rework dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,19 +1,60 @@
+
 class Dragon {
   constructor(name, rider) {
     this.name = name;
     this.rider = rider;
     this.hungry = true;
-    this.meals = 0
+    this.meal = 0;
   }
-  greet() {
+  greet(rider) {
     return `Hi, ${this.rider}!`
   }
   eat() {
-    this.meals++
-    if (this.meals >= 3) {
-      this.hungry = false;
-    }
+    this.meal++
+    if (this.meal >= 3) {
+    this.hungry = false;
   }
 }
+}
+
 
 module.exports = Dragon
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Dragon {
+//   constructor(name, rider) {
+//     this.name = name;
+//     this.rider = rider;
+//     this.hungry = true;
+//     this.meals = 0
+//   }
+//   greet() {
+//     return `Hi, ${this.rider}!`
+//   }
+//   eat() {
+//     this.meals++
+//     if (this.meals >= 3) {
+//       this.hungry = false;
+//     }
+//   }
+// }
