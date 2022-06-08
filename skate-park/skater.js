ed4c0dade2d6edf2d514c613c900020e47891a13
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2022-06-07 22:58:45 -0500

    Complete Fairy Test Suite

---
+++ skate-park/skater.js
@@ -0,0 +1,11 @@
+class Skater {
+  constructor(skaterDetails) {
+    this.name = skaterDetails.name;
+    this.skill = skaterDetails.skill;
+    this.tricks = skaterDetails.tricks;
+    this.money = ;
+  }
+}
+
+
+module.exports = Skater;
