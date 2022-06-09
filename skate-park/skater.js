f6cbb70c2e0185bb4f5df6b31d0d8ab80b233ae6
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2022-06-09 12:17:50 -0500

    complete skatepark test suite

--- skate-park/skater.js
+++ skate-park/skater.js
@@ -3,7 +3,19 @@ class Skater {
     this.name = skaterDetails.name;
     this.skill = skaterDetails.skill;
     this.tricks = skaterDetails.tricks;
-    this.money = ;
+    this.money = skaterDetails.cash;
+    this.frustration = 0;
+  }
+
+  practice(trick) {
+    if (!this.tricks[trick]) {
+    this.frustration ++;
+    }
+    if (this.frustration >= 3) {
+      this.tricks[trick] = true;
+      this.frustration = 0;
+      return `I just learned to ${trick}!!!`
+    }
   }
 }
 
