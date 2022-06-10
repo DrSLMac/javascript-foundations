971b5f3c143c6dce68d4eb60a5433190ba4ca955
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2022-06-09 22:43:21 -0500

    complete roadrace test suite

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,29 @@
-class Roadrace {
+const { Runner } = require("mocha");
 
+class Roadrace {
+    constructor(raceDetails) {
+        this.name = raceDetails.title;
+        this.location = raceDetails.city;
+        this.distance = raceDetails.distance;
+        this.participants = [];
+        
+    }
+
+    setDistance(distance) {
+        this.distance = distance;
+        return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+    }
+
+    registerParticipants(participants) {
+        this.participants.push(participants);
+    }
+
+    completeRace() {
+       for (var i = 0; i < this.participants.length; i++) {
+           this.participants[i].completedRaces.push(this.name);
+           this.participants[i].milesRun = 13.1;
+       }
+    }
 }
 
 module.exports = Roadrace;
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,26 @@
 class Runner {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.milesRun = 0;
+        this.fitness = 25;
+        this.completedRaces = [];
+    }
 
+    runSomeMiles(milesRun) {
+        this.milesRun += milesRun;
+        this.fitness += milesRun;
+    }
+
+    stretch() {
+        this.fitness += .5;
+    }
+
+    runRace(race, raceLength) {
+        this.completedRaces.push(race);
+        this.runSomeMiles(raceLength)
+       
+    }
 }
 
 module.exports = Runner;
